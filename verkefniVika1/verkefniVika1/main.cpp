2e45b2d6eec78cc1cad29cf1037456618cc5538f
nokkvikarlsson <nokkvi17@ru.is>
2017-11-27 14:59:43 +0000

    er ad testa

---
+++ verkefniVika1/verkefniVika1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  verkefniVika1
+//
+//  Created by Nokkvi Karlsson on 11/27/17.
+//  Copyright © 2017 Nokkvi Karlsson. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
